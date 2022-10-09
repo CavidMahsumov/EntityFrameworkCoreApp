64613cd0b603b0b3fc70cdea676b7aed39d224ea
Cavid <mehsumovcavid@gmail.com>
2022-10-09 15:39:44 +0400

    Add project files.

---
+++ Udemy.EfCore/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Udemy.EfCore.Data.Context;
+
+namespace Udemy.EfCore.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            UdemyContext context = new();
+
+            //add Product
+
+            //context.Products.Add(new Data.Entities.Product
+            //{
+            //       Name="Laptop",
+            //       Price=2000
+            //});
+
+            //UPDATE PRODUCT
+
+
+            //var updatedproduct=context.Products.Find(1);
+
+            //updatedproduct.Price = 4000;
+
+            //context.Products.Update(updatedproduct);
+
+
+            var deletedproduct = context.Products.FirstOrDefault(x => x.Id == 1);
+
+            context.Products.Remove(deletedproduct);
+
+            context.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Udemy.EfCore/Data/Context/UdemyContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Udemy.EfCore.Data.Entities;
+
+namespace Udemy.EfCore.Data.Context
+{
+    public class UdemyContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;database=UdemyEfCore; integrated security=true");
+        }
+      
+    }
+}
